7f697e0167a050be50b14a260bae3ab23b541ca6
MeePwn <maybewaityou@gmail.com>
2015-08-27 06:36:37 +0800

    first commit

---
+++ RAC_SinaProject/Classes/Main(主要)/Other/MPAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RAC_SinaProject
+//
+//  Created by ChunNan on 15/8/27.
+//  Copyright (c) 2015年 MeePwn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
