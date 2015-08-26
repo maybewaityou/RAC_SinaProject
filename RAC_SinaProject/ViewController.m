5ccb033caa5fdf7e1cc4dcc8919abee490381d0d
MeePwn <maybewaityou@gmail.com>
2015-08-27 05:20:13 +0800

    Initial Commit

---
+++ RAC_SinaProject/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RAC_SinaProject
+//
+//  Created by ChunNan on 15/8/27.
+//  Copyright (c) 2015年 MeePwn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ RAC_SinaProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RAC_SinaProject
+//
+//  Created by ChunNan on 15/8/27.
+//  Copyright (c) 2015年 MeePwn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
