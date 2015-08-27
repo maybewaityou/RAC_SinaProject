eff10298bfd74f31b034c5c58cbe1c99720500a8
MeePwn <maybewaityou@gmail.com>
2015-08-27 09:25:29 +0800

    first commit

---
+++ RAC_SinaProject/Classes/Main(主要)/Controller/MPTabBarViewController.m
@@ -0,0 +1,64 @@
+//
+//  MPTabBarViewController.m
+//  RAC_SinaProject
+//
+//  Created by ChunNan on 15/8/27.
+//  Copyright (c) 2015年 MeePwn. All rights reserved.
+//
+
+#import "MPTabBarViewController.h"
+#import "UIColor+Extension.h"
+#import "MPHomeViewController.h"
+#import "MPDiscoverViewController.h"
+#import "MPMessageCenterViewController.h"
+#import "MPProfileViewController.h"
+
+@interface MPTabBarViewController ()
+
+@end
+
+@implementation MPTabBarViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self setupViews];
+}
+
+- (void)setupViews
+{
+    MPHomeViewController *homeController = [[MPHomeViewController alloc] init];
+    MPMessageCenterViewController *messageController = [[MPMessageCenterViewController alloc] init];
+    MPDiscoverViewController *discoverController = [[MPDiscoverViewController alloc] init];
+    MPProfileViewController *profileController = [[MPProfileViewController alloc] init];
+    
+    [self setupController:homeController title:@"首页" image:@"tabbar_home" selectedImage:@"tabbar_home_selected"];
+    [self setupController:messageController title:@"消息" image:@"tabbar_message_center" selectedImage:@"tabbar_message_center_selected"];
+    [self setupController:discoverController title:@"发现" image:@"tabbar_discover" selectedImage:@"tabbar_discover_selected"];
+    [self setupController:profileController title:@"我的" image:@"tabbar_profile" selectedImage:@"tabbar_profile_selected"];
+    
+    
+    [self addChildViewController:homeController];
+    [self addChildViewController:messageController];
+    [self addChildViewController:discoverController];
+    [self addChildViewController:profileController];
+}
+
+- (void)setupController:(UIViewController *)controller title:(NSString *)title image:(NSString *)image selectedImage:(NSString *)selectedImage
+{
+    controller.tabBarItem.title = title;
+    controller.tabBarItem.image = [UIImage imageNamed:image];
+    controller.tabBarItem.selectedImage = [[UIImage imageNamed:selectedImage] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    
+    NSMutableDictionary *textAttrs = [NSMutableDictionary dictionary];
+    textAttrs[NSForegroundColorAttributeName] = [UIColor colorWithR:123 G:123 B:123];
+    NSMutableDictionary *selectedTexAttrs = [NSMutableDictionary dictionary];
+    selectedTexAttrs[NSForegroundColorAttributeName] = [UIColor orangeColor];
+    [controller.tabBarItem setTitleTextAttributes:textAttrs forState:UIControlStateNormal];
+    [controller.tabBarItem setTitleTextAttributes:selectedTexAttrs forState:UIControlStateSelected];
+    
+    controller.view.backgroundColor = [UIColor MPRandomColor];
+    
+}
+
+@end
--- RAC_SinaProject/Classes/Main(主要)/Other/MPAppDelegate.m
+++ RAC_SinaProject/Classes/Main(主要)/Other/MPAppDelegate.m
@@ -7,11 +7,7 @@
 //
 
 #import "MPAppDelegate.h"
-#import "MPHomeViewController.h"
-#import "MPDiscoverViewController.h"
-#import "MPMessageCenterViewController.h"
-#import "MPProfileViewController.h"
-#import "UIColor+Extension.h"
+#import "MPTabBarViewController.h"
 
 @interface MPAppDelegate ()
 
@@ -24,46 +20,13 @@
 
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     
-    UITabBarController *rootController = [[UITabBarController alloc] init];
+    MPTabBarViewController *rootController = [[MPTabBarViewController alloc] init];
     self.window.rootViewController = rootController;
-    
-    MPHomeViewController *homeController = [[MPHomeViewController alloc] init];
-    MPMessageCenterViewController *messageController = [[MPMessageCenterViewController alloc] init];
-    MPDiscoverViewController *discoverController = [[MPDiscoverViewController alloc] init];
-    MPProfileViewController *profileController = [[MPProfileViewController alloc] init];
-    
-    [self setupController:homeController title:@"首页" image:@"tabbar_home" selectedImage:@"tabbar_home_selected"];
-    [self setupController:messageController title:@"消息" image:@"tabbar_message_center" selectedImage:@"tabbar_message_center_selected"];
-    [self setupController:discoverController title:@"发现" image:@"tabbar_discover" selectedImage:@"tabbar_discover_selected"];
-    [self setupController:profileController title:@"我的" image:@"tabbar_profile" selectedImage:@"tabbar_profile_selected"];
-    
-    
-    [rootController addChildViewController:homeController];
-    [rootController addChildViewController:messageController];
-    [rootController addChildViewController:discoverController];
-    [rootController addChildViewController:profileController];
     
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyWindow];
     
     return YES;
-}
-
-- (void)setupController:(UIViewController *)controller title:(NSString *)title image:(NSString *)image selectedImage:(NSString *)selectedImage
-{
-    controller.tabBarItem.title = title;
-    controller.tabBarItem.image = [UIImage imageNamed:image];
-    controller.tabBarItem.selectedImage = [[UIImage imageNamed:selectedImage] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    
-    NSMutableDictionary *textAttrs = [NSMutableDictionary dictionary];
-    textAttrs[NSForegroundColorAttributeName] = [UIColor colorWithR:123 G:123 B:123];
-    NSMutableDictionary *selectedTexAttrs = [NSMutableDictionary dictionary];
-    selectedTexAttrs[NSForegroundColorAttributeName] = [UIColor orangeColor];
-    [controller.tabBarItem setTitleTextAttributes:textAttrs forState:UIControlStateNormal];
-    [controller.tabBarItem setTitleTextAttributes:selectedTexAttrs forState:UIControlStateSelected];
-    
-    controller.view.backgroundColor = [UIColor MPRandomColor];
-    
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
