d34c5383c4ca593824b41b8155fb801c7dada42c
MeePwn <maybewaityou@gmail.com>
2015-09-08 11:08:46 +0800

    first commit

--- RAC_SinaProject/Classes/Home(首页)/Model/Status.m
+++ RAC_SinaProject/Classes/Home(首页)/Model/Status.m
@@ -9,12 +9,48 @@
 #import "Status.h"
 #import "MJExtension.h"
 #import "MPPhoto.h"
+#import "NSDate+Extension.h"
 
 @implementation Status
 
 - (NSDictionary *)objectClassInArray
 {
     return @{@"pic_urls" : [MPPhoto class]};
+}
+
+- (NSString *)created_at
+{
+    NSDateFormatter *fmt = [[NSDateFormatter alloc] init];
+    fmt.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US"];
+    fmt.dateFormat = @"EEE MMM dd HH:mm:ss Z yyyy";
+    
+    NSDate *createdDate = [fmt dateFromString:_created_at];
+    NSDate *now = [NSDate date];
+    
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSCalendarUnit unit = NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay | NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond;
+    NSDateComponents *cmps = [calendar components:unit fromDate:createdDate toDate:now options:0];
+    
+    if ([createdDate isThisYear]) {
+        if ([createdDate isYesterday]) {
+            fmt.dateFormat = @"昨天 HH:mm";
+            return [fmt stringFromDate:createdDate];
+        }else if([createdDate isToday]){
+            if (cmps.hour >= 1) {
+                return [NSString stringWithFormat:@"%zd小时前",cmps.hour];
+            }else if(cmps.minute >= 1){
+                return [NSString stringWithFormat:@"%zd分钟前",cmps.minute];
+            }else{
+                return @"刚刚";
+            }
+        }else {
+            fmt.dateFormat = @"MM-dd HH:mm";
+            return [fmt stringFromDate:createdDate];
+        }
+    }else {
+        fmt.dateFormat = @"yyyy-MM-dd HH:mm";
+        return [fmt stringFromDate:createdDate];
+    }
 }
 
 - (NSString *)description
