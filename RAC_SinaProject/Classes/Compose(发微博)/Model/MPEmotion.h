206445b9aab6b364fd4041ed4bf482ee1b47d2be
MeePwn <maybewaityou@gmail.com>
2015-09-17 15:33:37 +0800

    first commit

---
+++ RAC_SinaProject/Classes/Compose(发微博)/Model/MPEmotion.h
@@ -0,0 +1,18 @@
+//
+//  MPEmotion.h
+//  RAC_SinaProject
+//
+//  Created by ChunNan on 15/9/17.
+//  Copyright (c) 2015年 MeePwn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MPEmotion : NSObject
+
+@property (nonatomic, copy) NSString *chs;
+@property (nonatomic, copy) NSString *png;
+/** emoji表情的16进制编码 */
+@property (nonatomic, copy) NSString *code;
+
+@end
---
+++ RAC_SinaProject/Classes/Compose(发微博)/Model/MPEmotion.m
@@ -0,0 +1,13 @@
+//
+//  MPEmotion.m
+//  RAC_SinaProject
+//
+//  Created by ChunNan on 15/9/17.
+//  Copyright (c) 2015年 MeePwn. All rights reserved.
+//
+
+#import "MPEmotion.h"
+
+@implementation MPEmotion
+
+@end
