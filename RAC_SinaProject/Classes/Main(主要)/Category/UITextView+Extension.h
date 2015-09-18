f84a2c2f8204404dbcda10825389614074e006b7
MeePwn <maybewaityou@gmail.com>
2015-09-19 06:03:08 +0800

    first commit

--- RAC_SinaProject/Classes/Main(主要)/Category/UITextView+Extension.h
+++ RAC_SinaProject/Classes/Main(主要)/Category/UITextView+Extension.h
@@ -10,6 +10,7 @@
 
 @interface UITextView (Extension)
 
-- (void)insertAttributeText:(NSAttributedString *)text;
+- (void)insertAttributedText:(NSAttributedString *)text;
+- (void)insertAttributedText:(NSAttributedString *)text settingBlock:(void (^)(NSMutableAttributedString *attributedText))settingBlock;
 
 @end
--- RAC_SinaProject/Classes/Main(主要)/Category/UITextView+Extension.m
+++ RAC_SinaProject/Classes/Main(主要)/Category/UITextView+Extension.m
@@ -10,15 +10,41 @@
 
 @implementation UITextView (Extension)
 
-- (void)insertAttributeText:(NSAttributedString *)text
+//- (void)insertAttributeText:(NSAttributedString *)text
+//{
+//    NSMutableAttributedString *attributedText = [[NSMutableAttributedString alloc] init];
+//    [attributedText appendAttributedString:self.attributedText];
+//    NSUInteger loc = self.selectedRange.location;
+//    [attributedText insertAttributedString:text atIndex:loc];
+//    
+//    self.attributedText = attributedText;
+//    
+//    self.selectedRange = NSMakeRange(loc + 1, 0);
+//}
+
+- (void)insertAttributedText:(NSAttributedString *)text
+{
+    [self insertAttributedText:text settingBlock:nil];
+}
+
+- (void)insertAttributedText:(NSAttributedString *)text settingBlock:(void (^)(NSMutableAttributedString *))settingBlock
 {
     NSMutableAttributedString *attributedText = [[NSMutableAttributedString alloc] init];
+    // 拼接之前的文字（图片和普通文字）
     [attributedText appendAttributedString:self.attributedText];
+    
+    // 拼接其他文字
     NSUInteger loc = self.selectedRange.location;
     [attributedText insertAttributedString:text atIndex:loc];
     
+    // 调用外面传进来的代码
+    if (settingBlock) {
+        settingBlock(attributedText);
+    }
+    
     self.attributedText = attributedText;
     
+    // 移除光标到表情的后面
     self.selectedRange = NSMakeRange(loc + 1, 0);
 }
 
