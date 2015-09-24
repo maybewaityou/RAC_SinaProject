c8e41f3b8de9bc1e8e16b123b9ad62528bbe0008
MeePwn <maybewaityou@gmail.com>
2015-09-24 11:16:01 +0800

    first commit

--- RAC_SinaProject/Classes/Compose(发微博)/Model/MPEmotion.m
+++ RAC_SinaProject/Classes/Compose(发微博)/Model/MPEmotion.m
@@ -8,6 +8,28 @@
 
 #import "MPEmotion.h"
 
+@interface MPEmotion () <NSCoding>
+
+@end
+
 @implementation MPEmotion
+
+- (instancetype)initWithCoder:(NSCoder *)decoder
+{
+    self = [super init];
+    if (self) {
+        self.chs = [decoder decodeObjectForKey:@"chs"];
+        self.png = [decoder decodeObjectForKey:@"png"];
+        self.code = [decoder decodeObjectForKey:@"code"];
+    }
+    return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)encoder
+{
+    [encoder encodeObject:self.chs forKey:@"chs"];
+    [encoder encodeObject:self.png forKey:@"png"];
+    [encoder encodeObject:self.code forKey:@"code"];
+}
 
 @end
