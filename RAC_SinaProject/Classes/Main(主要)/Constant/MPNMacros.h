a2f6fc0ac3c9a025824ae1ba25339d8eb7c6c375
MeePwn <maybewaityou@gmail.com>
2015-09-29 06:52:22 +0800

    first commit

--- RAC_SinaProject/Classes/Main(主要)/Constant/MPNMacros.h
+++ RAC_SinaProject/Classes/Main(主要)/Constant/MPNMacros.h
@@ -13,6 +13,12 @@
 #define SCREEN_WIDTH ([UIScreen mainScreen].bounds.size.width)
 #define SCREEN_HEIGHT ([UIScreen mainScreen].bounds.size.height)
 
+#define MPStatusPhotoWH ((SCREEN_WIDTH - 40) / 3.0)
+#define margin5 (5)
+#define margin10 (10)
+#define defaultW (44)
+#define defaultH (44)
+
 #define XCODE_COLORS_RESET_FG  XCODE_COLORS_ESCAPE @"fg;" // Clear any foreground color
 #define XCODE_COLORS_RESET_BG  XCODE_COLORS_ESCAPE @"bg;" // Clear any background color
 #define XCODE_COLORS_RESET     XCODE_COLORS_ESCAPE @";"   // Clear any foreground or background color
