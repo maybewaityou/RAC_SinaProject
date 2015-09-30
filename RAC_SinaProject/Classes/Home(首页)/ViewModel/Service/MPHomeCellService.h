07c65970780cc590e72052b14b9ee85c28646f22
MeePwn <maybewaityou@gmail.com>
2015-10-01 06:25:46 +0800

    first commit

---
+++ RAC_SinaProject/Classes/Home(首页)/ViewModel/Service/MPHomeCellService.h
@@ -0,0 +1,16 @@
+//
+//  MPHomeCellService.h
+//  RAC_SinaProject
+//
+//  Created by ChunNan on 15/10/1.
+//  Copyright © 2015年 MeePwn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ModelServices.h"
+
+@interface MPHomeCellService : NSObject <ModelServices>
+
+- (instancetype)initWithNavController:(UINavigationController *) controller;
+
+@end
---
+++ RAC_SinaProject/Classes/Home(首页)/ViewModel/Service/MPHomeCellService.m
@@ -0,0 +1,50 @@
+//
+//  MPHomeCellService.m
+//  RAC_SinaProject
+//
+//  Created by ChunNan on 15/10/1.
+//  Copyright © 2015年 MeePwn. All rights reserved.
+//
+
+#import "MPHomeCellService.h"
+#import "MPNetworkServicesImpl.h"
+
+@interface MPHomeCellService ()
+
+@property (nonatomic, strong) MPNetworkServicesImpl *service;
+@property (nonatomic, weak) UIViewController *controller;
+
+@end
+
+@implementation MPHomeCellService
+
+- (instancetype)initWithNavController:(UINavigationController *) controller
+{
+    if (self = [super init]) {
+        _controller = controller;
+        [self initialize];
+    }
+    return self;
+}
+
+- (void)initialize
+{
+    _service = [[MPNetworkServicesImpl alloc] init];
+}
+
+- (id<ModelNetwork>)getNetworkService
+{
+    return self.service;
+}
+
+- (void)pushViewModel:(id)viewModel
+{
+    
+}
+
+- (void)dealloc
+{
+    NSLog(@"===>>> %@ dealloc",self);
+}
+
+@end
