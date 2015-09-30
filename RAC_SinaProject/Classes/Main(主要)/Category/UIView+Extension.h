4412ca72627f95883b8c03b9df3e71e77ca27c55
MeePwn <maybewaityou@gmail.com>
2015-09-30 15:23:18 +0800

    first commit

--- RAC_SinaProject/Classes/Main(主要)/Category/UIView+Extension.h
+++ RAC_SinaProject/Classes/Main(主要)/Category/UIView+Extension.h
@@ -10,13 +10,13 @@
 
 @interface UIView (Extension)
 
-@property (nonatomic, assign)CGFloat x;
-@property (nonatomic, assign)CGFloat y;
-@property (nonatomic, assign)CGFloat width;
-@property (nonatomic, assign)CGFloat height;
-@property (nonatomic, assign)CGSize size;
-@property (nonatomic, assign)CGPoint origin;
-@property (nonatomic, assign)CGFloat centerX;
-@property (nonatomic, assign)CGFloat centerY;
+@property (nonatomic, assign) CGFloat x;
+@property (nonatomic, assign) CGFloat y;
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
+@property (nonatomic, assign) CGSize size;
+@property (nonatomic, assign) CGPoint origin;
+@property (nonatomic, assign) CGFloat centerX;
+@property (nonatomic, assign) CGFloat centerY;
 
 @end
