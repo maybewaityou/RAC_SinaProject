cf304b5f573234b01b629e27faf140acbcb46c44
MeePwn <maybewaityou@gmail.com>
2015-10-02 12:47:32 +0800

    first commit

---
+++ RAC_SinaProject/Classes/Home(首页)/Tools/MPStatusTool.h
@@ -0,0 +1,17 @@
+//
+//  MPStatusTool.h
+//  RAC_SinaProject
+//
+//  Created by ChunNan on 15/10/2.
+//  Copyright © 2015年 MeePwn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class Status;
+
+@interface MPStatusTool : NSObject
+
+
+
+@end
---
+++ RAC_SinaProject/Classes/Home(首页)/Tools/MPStatusTool.m
@@ -0,0 +1,13 @@
+//
+//  MPStatusTool.m
+//  RAC_SinaProject
+//
+//  Created by ChunNan on 15/10/2.
+//  Copyright © 2015年 MeePwn. All rights reserved.
+//
+
+#import "MPStatusTool.h"
+
+@implementation MPStatusTool
+
+@end
