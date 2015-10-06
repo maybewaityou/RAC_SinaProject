b9624e311aa80f4ef534f685b97cea9939b2aeec
MeePwn <maybewaityou@gmail.com>
2015-10-06 16:30:11 +0800

    first commit

--- RAC_SinaProject/Classes/Home(首页)/Tools/MPStatusTool.m
+++ RAC_SinaProject/Classes/Home(首页)/Tools/MPStatusTool.m
@@ -8,19 +8,51 @@
 
 #import "MPStatusTool.h"
 #import "Status.h"
+#import "FMDB.h"
+#import "Constant.h"
 
 @implementation MPStatusTool
 
+static FMDatabase *_db;
+
++ (void)initialize
+{
+    NSString *path = [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject] stringByAppendingPathComponent:@"statuses.sqlite"];
+    
+    _db = [[FMDatabase alloc] initWithPath:path];
+    [_db open];
+    
+    [_db executeUpdate:MPCreateTableSQL];
+}
+
 + (NSArray *)statusWithParams:(NSDictionary *)params;
 {
+    NSString *sql = nil;
+    if (params[@"since_id"]) {
+        sql = [NSString stringWithFormat:@"SELECT * FROM t_status WHERE idstr > %@ ORDER BY idstr DESC LIMIT 20;",params[@"since_id"]];
+    }else if(params[@"max_id"]){
+        sql = [NSString stringWithFormat:@"SELECT * FROM t_status WHERE idstr <= %@ ORDER BY idstr DESC LIMIT 20;",params[@"max_id"]];
+    }else{
+        sql = MPQueryStatusSQL;
+    }
     
+    FMResultSet *set = [_db executeQuery:sql];
+    NSMutableArray *statuses = [NSMutableArray array];
+    while (set.next) {
+        NSData *data = [set objectForColumnName:@"status"];
+        NSDictionary *status = [NSKeyedUnarchiver unarchiveObjectWithData:data];
+        [statuses addObject:status];
+    }
     
-    return nil;
+    return statuses;
 }
 
 + (void)saveStatuses:(NSArray *)statuses
 {
-    
+    for (NSDictionary *status in statuses) {
+        NSData *statusData = [NSKeyedArchiver archivedDataWithRootObject:status];
+        [_db executeUpdateWithFormat:@"INSERT INTO t_status(status, idstr) VALUES (%@, %@)", statusData, status[@"idstr"]];
+    }
 }
 
 @end
